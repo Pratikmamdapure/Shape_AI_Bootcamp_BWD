e678f910e7f834dcd3a41b6e07be8ed49b5c6d69
Pratikmamdapure <85956467+Pratikmamdapure@users.noreply.github.com>
2021-06-27 13:51:18 +0530

    Add files via upload

---
+++ Info4.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info4(){
+  return(
+  <div className="note">
+     <h1>Python & Network Security - Free 7 Days Long Bootcamp</h1>
+     <p>Here we bring our another free live BOOTCAMP with a hands-on project at the end; Python and Networking Security</p>
+  </div>
+  ) ;
+}
+export default Info4;
